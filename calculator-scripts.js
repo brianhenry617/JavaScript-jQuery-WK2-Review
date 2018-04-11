3b416715d0b779e988a9bbd115bff37023aa0258
Brian Henry <94brianhenry@gmail.com>
2018-04-10 20:41:21 -0700

    Initial Commit

---
+++ calculator-scripts.js
@@ -0,0 +1,22 @@
+// Business Logic
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subract = function(number1, number2) {
+  return number1 - number2;
+};
+var product = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+
+// User Interface Logic
+var answer = answer
